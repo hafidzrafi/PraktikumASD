720e24143a1f1ccd3752d2a915be1d2579b6262c
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2026-02-22 20:03:32 +0700

    Jobsheet 2 Percobaan 1

---
+++ Jobsheet2/Mahasiswa19.java
@@ -0,0 +1,46 @@
+package Jobsheet2;
+
+public class Mahasiswa19 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa19(String nama, String nim, String kelas, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0 && ipkBaru <= 4) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
