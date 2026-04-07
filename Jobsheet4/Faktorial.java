dd85b6160913f5566423209fee555fd7a10e9134
Hafidz Rafi Rabbani <sapineptwn@gmail.com>
2026-04-07 11:05:07 +0700

    Jobsheet 4

---
+++ Jobsheet4/Faktorial.java
@@ -0,0 +1,30 @@
+package Jobsheet4;
+
+public class Faktorial {
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    int faktorialBFWhile(int n) {
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto * i;
+             i++;
+        }
+        return fakto;
+    }
+
+    int faktorialDC(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
+    }
+}
